07d22237d8bb627d381cb401000d1ba940b31b85
Parminder <parminder@ezzie.in>
2016-07-30 08:07:20 +0530

    push

--- scripts/contact.php
+++ scripts/contact.php
@@ -3,7 +3,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
 	$to = 'info@wcarps.com'; // Your e-mail address here.
 	$body = "\nName: {$_POST['name']}\nEmail: {$_POST['email']}\n\n\n{$_POST['message']}\n\n";
-	mail($to, "Message from yoursite.com", $body, "From: {$_POST['email']}"); // E-Mail subject here.
+	$subject = "Message from wcarps.com";
+
+	$headers = "From: $_POST['email']\r\n";
+    $headers .= "Reply-To: info@wcarps.com\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+
+    mail($to,$subject,$body,$headers);		
+
+		
     }
 }
 ?>
