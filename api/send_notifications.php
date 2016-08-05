7c44ad3355209e87a7d1d9356061d2296b8b255a
Parminder Singh <parminder@ezzie.in>
2016-08-05 16:02:32 +0530

    psh

--- api/send_notifications.php
+++ api/send_notifications.php
@@ -40,7 +40,7 @@ if(isset($_POST['vehicleID']) && isset($_POST['location']) && isset($_POST['dol'
 		$message .= "<li><strong>Chassis No.:</strong> " . $vehicleDetails['chassis_no']. "</li>";
 		$message .= "<li><strong>Status:</strong> Lost</li>";
 		$message .= "<li><strong>Location:</strong> " . $location . "</li>";
-		$message .= "<li><strong>Date/Time:</strong> " .date_format($dlost,'d/m/Y H:i:s') . "</li>";
+		$message .= "<li><strong>Date/Time:</strong> " .date_format($dlost,'d/m/Y H:i A') . "</li>";
 
 		$message .= "</ul>";
 
